848f968de84b9ea63024a9ff449ac10419671baa
JosephShin <joespam9999@gmail.com>
2022-04-11 10:43:48 -0400

    commit tree 198f43afda82b15b6d53d57cf8ce251b0ffd8a46

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let changeLine = document.querySelector('#text')
+    changeLine.textContent = 'This is really cool!'
+})
